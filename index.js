4a09897d1a0492a7192f26d2510e06fb38d77d10
aren1985 <arenh1985@gmail.com>
2024-12-22 22:15:09 +0400

    vay vay

--- index.js
+++ index.js
@@ -11,23 +11,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:3000", // Local development (adjust if necessary)
-  "https://hyeng.vercel.app/", // Production domain on Vercel
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow requests with matching origins
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 const mongourl = process.env.MONGOURL;
